25d3d72717536f5c7b939b9525675120e9f16894
RadomirRZ <kingradomir@gmail.com>
2022-12-11 01:45:57 +0600

    'qwerty'

--- bot_instance.py
+++ bot_instance.py
@@ -1,10 +1,10 @@
-from decouple import config
 from aiogram import Bot, Dispatcher
+import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
+from dotenv import load_dotenv
 storage = MemoryStorage()
+load_dotenv()
 
-
-TOKEN = config("TOKEN")
+TOKEN = os.getenv('TOKEN')
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
